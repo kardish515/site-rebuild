d214c166f7e3afc1ecd20c1a9f53bde5dafcee42
Nick Kardish <kardish515@gmail.com>
2018-05-04 09:58:39 -0700

    create components news-stories and opinion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NewsStoriesComponent } from './news-stories/news-stories.component';
+import { OpinionComponent } from './opinion/opinion.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    NavbarComponent,
+    NewsStoriesComponent,
+    OpinionComponent
   ],
   imports: [
     BrowserModule
