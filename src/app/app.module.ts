28f8007ae3398c179e9d35896a7c7f3fc32a2490
Nick Kardish <kardish515@gmail.com>
2018-05-11 08:30:16 -0700

    add firebase database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -10,7 +8,16 @@ import { OpinionComponent } from './opinion/opinion.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AdvertisementComponent } from './advertisement/advertisement.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -24,7 +31,9 @@ import { AdvertisementComponent } from './advertisement/advertisement.component'
     AdvertisementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
